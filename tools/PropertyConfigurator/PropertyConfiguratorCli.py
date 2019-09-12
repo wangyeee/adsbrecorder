c4a1cb85b9fdb793a480b155b01771dbbd510a2a
U-YE-AW15R3\Ye <wangyeee@gmail.com>
2019-09-12 21:02:32 +1200

    Add commandline version of PropertyConfigurator

---
+++ tools/PropertyConfigurator/PropertyConfiguratorCli.py
@@ -0,0 +1,113 @@
+import os, sys
+import json
+from pathlib import Path
+from jproperties import Properties
+
+class PropScanner:
+
+    def __init__(self, folder):
+        self.folder = folder
+        self.propsFileList = []
+        self.excludePath = [
+            os.path.sep + 'target' + os.path.sep + 'classes' + os.path.sep,
+            os.path.sep + 'target' + os.path.sep + 'test-classes' + os.path.sep
+        ]
+
+    def scan(self):
+        pathList = Path(self.folder).glob('**/*.properties.template')
+        for path in pathList:
+            pathStr = str(path)
+            if self.includePath(pathStr):
+                self.propsFileList.append(pathStr)
+
+    def includePath(self, path):
+        for excl in self.excludePath:
+            if excl in path:
+                return False
+        return True
+
+class PropertyTemplate:
+
+    def __init__(self, path):
+        self.config = Properties()
+        self.path = path
+        self.destPath = path[0:len(path)-len('.template')]
+        with open(path, 'rb') as f:
+            self.config.load(f, 'utf-8')
+        self.existingProps = {}
+        self.__checkExistingProps(self.destPath)
+
+    def __checkExistingProps(self, extPropsPath):
+        try:
+            existConfig = Properties()
+            with open(extPropsPath, 'rb') as f:
+                existConfig.load(f, 'utf-8')
+                self.existingProps.update(existConfig.properties)
+        except FileNotFoundError:
+            for key, val in self.config.properties.items():
+                if val != 'CHANGEME':
+                    self.existingProps[key] = val
+
+    def getParametersNotSet(self):
+        params = {}
+        for key, val in self.config.properties.items():
+            if val == 'CHANGEME':
+                params[key] = '' if key not in self.existingProps else self.existingProps[key]
+        return params
+
+    def getOutputPropertiesFileName(self):
+        return self.destPath
+
+    def updateExistingProperty(self, key, value):
+        self.existingProps[key] = value
+
+    def updateExistingProperties(self, props):
+        self.existingProps.update(props)
+
+    def generatePropertiesFile(self):
+        p = Properties()
+        p.properties.update(self.existingProps)
+        with open(self.destPath, 'wb') as f:
+            p.store(f, encoding='utf-8')
+
+def scan(scanPath):
+    scanner = PropScanner(scanPath)
+    scanner.scan()
+    templateList = []
+    for tmpl in scanner.propsFileList:
+        q = PropertyTemplate(tmpl)
+        lst = q.getParametersNotSet()
+        notSet = {}
+        for l in lst:
+            notSet[l] = ''
+        if len(notSet) > 0:
+            template = {}
+            template['file'] = q.getOutputPropertiesFileName()
+            template['properties'] = notSet
+            templateList.append(template)
+        else:
+            q.generatePropertiesFile()
+    with open(scanPath + os.path.sep + 'manual_config.json', 'wb') as manCfg:
+        manCfg.write(json.dumps(templateList, indent=4).encode('utf-8'))
+        print('Please edit manual_config.json in ' + scanPath + ' to set parameters without default values.')
+        print('And then generate properties files with `python ' + os.path.basename(__file__) + ' manual_config.json`')
+
+def generateFromManualConfig(configPath):
+    with open(configPath, 'rb') as manCfg:
+        configList = json.load(manCfg)
+        for config in configList:
+            tmplCfg = PropertyTemplate(config['file'] + '.template')
+            tmplCfg.updateExistingProperties(config['properties'])
+            tmplCfg.generatePropertiesFile()
+
+def main():
+    srcPath = os.path.dirname(os.path.realpath(__file__))
+    if len(sys.argv) == 2:
+        if os.path.isfile(sys.argv[1]):
+            generateFromManualConfig(os.path.abspath(sys.argv[1]))
+            return
+        srcPath = os.path.abspath(sys.argv[1])
+    scan(srcPath)
+
+if __name__ == '__main__':
+    main()
